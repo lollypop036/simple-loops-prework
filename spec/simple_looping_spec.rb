d4a8e7d39fbe29f01f15162a301a3190b897fcf8
Lola Onanuga <slonanuga@gmail.com>
2018-09-21 15:09:01 +0000

    automatically backed up by learn

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -1,9 +1,10 @@
 require_relative './spec_helper'
 
-describe 'Loops' do
+describe 'Loop' do
   before(:each) do
     @looping_string = "Welcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\n"
   end
+
 
   describe 'loop' do
     it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
