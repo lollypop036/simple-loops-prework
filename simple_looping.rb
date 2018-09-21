eca4ea2ba2680ccb78a9a3232058473f9ff357c8
Lola Onanuga <slonanuga@gmail.com>
2018-09-21 15:09:18 +0000

    automatically backed up by learn

---
+++ simple_looping.rb
@@ -0,0 +1,49 @@
+def loop_iterator(number_of_times)
+  counter = 0
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  loop do
+    puts phrase
+    counter +=1
+    break if counter == number_of_times
+  end
+    
+end
+
+def times_iterator(number_of_times)
+  
+   phrase = "Welcome to Flatiron School's Web Development Course!"
+   number_of_times.times do
+   puts phrase
+ 
+end
+end
+
+def while_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
+  while counter != number_of_times
+ puts phrase
+ counter +=1
+ end
+end
+
+def until_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
+  until counter == number_of_times
+  
+  puts phrase
+  counter +=1
+  end
+end
+
+def for_iterator(number_of_times)
+  
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times = 1..number_of_times
+  for i in number_of_times
+  puts phrase
+end
+  
+end
+
